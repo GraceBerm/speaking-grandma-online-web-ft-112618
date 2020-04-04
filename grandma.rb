43e0155ed4a269794555bc1e35f43431a2faad40
Grace Berman <grace.g.berman@gmail.com>
2020-04-04 15:17:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,13 @@
-def speak_to_grandma(phrase)
+def correct_phrase_for(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
      "NO, NOT SINCE 1938!"
+   end
 end
-end
+
+
+x =  "Hey grandma whats up?"
+puts x
